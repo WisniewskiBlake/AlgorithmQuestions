1710e7e623afeb4d575130401c03d01927963019
WisniewskiBlake <wisniewski.blake@gmail.com>
2020-12-17 01:15:58 -0500

    remove duplicates

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        ValidParentheses romanToInt = new ValidParentheses();
-        System.out.println(romanToInt.isValid("(("));
+        RemoveDuplicates romanToInt = new RemoveDuplicates();
+        int[] nums = {0,0,1,1,1,2};
+        System.out.println(romanToInt.removeDuplicates(nums));
     }
 }
